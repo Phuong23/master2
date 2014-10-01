52ac1084acfda19623c3a159cbb76570bc404eca
personne <pierre.boudes@lipn.univ-paris13.fr>
2014-10-02 01:18:56 +0200

    LEE tp1 q 4

--- java/tp1/Client.java
+++ java/tp1/Client.java
@@ -1,4 +1,7 @@
 import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class Client {
@@ -9,9 +12,20 @@ public class Client {
     public Client () {
 	Log.p("Trying to reach " + Client.host + " on port " + Client.port);
 	try (
-	    Socket connexionSocket = new Socket(Client.host, Client.port)
+	    Socket connexionSocket = new Socket(Client.host, Client.port);
+	    PrintWriter toServer = new PrintWriter(connexionSocket.getOutputStream(), true);
+	    BufferedReader fromServer = new BufferedReader(new InputStreamReader(connexionSocket.getInputStream()));
+	    BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in))
 	) {
 	    Log.p("Connecté !");
+	    String consoleInput, serverInput;
+	    while ((consoleInput = fromConsole.readLine()) != null) {
+		toServer.println(consoleInput);
+		Log.p("to Server : " + consoleInput);
+		serverInput = fromServer.readLine();
+		if (serverInput == null) break;
+		Log.p("from Server : " + serverInput);
+	    }
 	}
 	catch (IOException e) { Log.p(e); }
     }
--- java/tp1/Server.java
+++ java/tp1/Server.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Server {
@@ -14,14 +17,48 @@ public class Server {
 	Log.p("Listen on port " + Server.port);
 	try (
 	    ServerSocket serverSocket = new ServerSocket(Server.port);
-	    Socket clientSocket = serverSocket.accept()
+	    Socket clientSocket = serverSocket.accept();
+	    PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true); // true for autoflush
+	    BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
 	) {
 	    Log.p("Client connecté !");
+	    String clientInput;
+	    int ageFound;
+	    while ((clientInput = fromClient.readLine()) != null) {
+		Log.p("from Client " + clientInput);
+		try {
+		    ageFound = this.searchWithNom(clientInput).getAge();
+		    toClient.println(ageFound);
+		    Log.p("to Client " + ageFound);
+		}
+		catch (EnseignantNotFoundException e) {
+		    toClient.println("Not found");
+		    Log.p("to Client : Not found");
+		}
+	    }
 	}
 	catch (IOException e) { Log.p(e); }
     }
 
-    public boolean initEnseignantsL () { this.enseignantsL = new ArrayList<Enseignant>(); return true; }
+    public Enseignant searchWithNom (String nom) throws EnseignantNotFoundException {
+	for (Enseignant ens : this.enseignantsL) {
+	    if (ens.getNom().compareToIgnoreCase(nom) == 0) {
+		Log.p("Found : " + nom);
+		return ens;
+	    }
+	}
+	throw new EnseignantNotFoundException();
+    }
+
+    class EnseignantNotFoundException extends Exception {}; 
+
+    public boolean initEnseignantsL () { 
+	this.enseignantsL = new ArrayList<Enseignant>(); 
+	this.enseignantsL.add(new Enseignant("Kadabra", 77, 1300, Grade.MCU));
+	this.enseignantsL.add(new Enseignant("Ghost Dog", 33, 5000, Grade.PRAG));
+	this.enseignantsL.add(new Enseignant("Cheminade", 66, 3000, Grade.BIATTS));
+	return true;
+    }
 
     public static void main (String[] args) {
 	// Launch
